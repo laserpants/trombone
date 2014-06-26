f7477151a31d117edf006c6cef0021eb88aff7c7
johanneshilden <hildenjohannes@gmail.com>
2014-06-26 13:52:51 +0200

    added logging middleware

---
+++ Trombone/Middleware/Logger.hs
@@ -0,0 +1,17 @@
+module Trombone.Middleware.Logger 
+    ( module Network.Wai.Middleware.RequestLogger
+    , buildLogger
+    ) where
+
+import Data.Default
+import Network.Wai                                     ( Middleware )
+import Network.Wai.Middleware.RequestLogger
+import System.Log.FastLogger
+
+buildLogger :: BufSize        -- ^ Buffer size
+            -> FilePath       -- ^ Log file location
+            -> IO Middleware
+buildLogger bufsize path = do
+    file <- newFileLoggerSet bufsize path
+    mkRequestLogger def { destination = Logger file }
+
