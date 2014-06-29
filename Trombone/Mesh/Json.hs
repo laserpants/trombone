a179d3e6179048234f3dcf580d6961874f203188
johanneshilden <hildenjohannes@gmail.com>
2014-06-29 16:39:22 +0200

    re-adding Mesh dir

---
+++ Trombone/Mesh/Json.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE OverloadedStrings, TypeSynonymInstances #-}
+module Trombone.Mesh.Json where
+
+import Control.Applicative
+import Control.Monad
+import Data.Aeson
+import Data.Scientific
+import Network.HTTP.Types                              
+import Trombone.Mesh
+
+import qualified Data.HashMap.Strict                   as HMS
+import qualified Data.ByteString.Lazy.Char8            as L8
+
+instance FromJSON ProcessorId where
+    parseJSON (Number n) = return $ Id $ fromIntegral $ coefficient n
+    parseJSON _          = mzero
+
+instance FromJSON Predicate where
+    parseJSON (String "equalTo")            = return PredEqualTo
+    parseJSON (String "notEqualTo")         = return PredNotEqualTo
+    parseJSON (String "greaterThan")        = return PredGreaterThan
+    parseJSON (String "greaterThanOrEqual") = return PredGreaterThanOrEqual
+    parseJSON (String "lessThan")           = return PredLessThan
+    parseJSON (String "lessThanOrEqual")    = return PredLessThanOrEqual
+    parseJSON _ = mzero
+
+instance FromJSON TransType where
+    parseJSON (String "exclude")            = return TransExclude
+    parseJSON (String "include")            = return TransInclude
+    parseJSON (String "bind")               = return TransBind
+    parseJSON (String "rename")             = return TransRename
+    parseJSON _ = mzero
+
+instance FromJSON Filter where
+    parseJSON (Object o)
+        = Filter <$> o .: "property"
+                 <*> o .: "predicate"
+                 <*> o .: "value"
+    parseJSON _ = mzero
+
+instance FromJSON Transformer where
+    parseJSON (Object o)
+        = Transformer <$> o .: "action"
+                      <*> o .: "arguments"
+    parseJSON _ = mzero
+
+instance FromJSON Connection where
+    parseJSON (Object o) 
+        = Connection <$> o .:? "source"        .!= In
+                     <*> o .:? "destination"   .!= Out
+                     <*> o .:  "transformers" 
+                     <*> o .:  "filters" 
+    parseJSON _ = mzero
+
+instance FromJSON Method where
+    parseJSON (String "GET")     = return "GET"
+    parseJSON (String "POST")    = return "POST"
+    parseJSON (String "PUT")     = return "PUT"
+    parseJSON (String "DELETE")  = return "DELETE"
+    parseJSON (String "PATCH")   = return "PATCH"
+    parseJSON _                  = mzero
+
+instance FromJSON Processor where
+    parseJSON (Object o)
+        = Processor <$> o .:  "id"
+                    <*> o .:  "method"
+                    <*> o .:  "uri"
+                    <*> o .:? "expand"
+    parseJSON _ = mzero
+
+instance FromJSON System where
+    parseJSON (Object o)
+        = System <$> o .: "processors"
+                 <*> o .: "connections"
+                 <*> return []
+    parseJSON _ = mzero
+
+parseMeshFromFile :: FilePath -> IO [(Text, System)]
+parseMeshFromFile file = do
+    f <- readFile file
+    return $ case decode (L8.pack f) of
+               Nothing -> []
+               Just p  -> HMS.toList p
+
